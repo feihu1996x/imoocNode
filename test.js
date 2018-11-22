584d750f30ffe84d1070ff2561628f1c59e97e76
feihu1996x <gsx0312@qq.com>
2018-11-22 21:15:48 +0800

    先从一个牛逼闪闪的知识点 Promise 讲起

--- test.js
+++ test.js
@@ -1,12 +1,16 @@
-(function (){
-    console.log('A');
-    (function(){
-        console.log('B');
-        (function(){
-            console.log('C');
-            (function(){
-                console.log('D');
-            })();
-        })();
-    })();
-})();
+let ajax=function(){
+    console.log('执行2');
+    // resolve参数表示要执行下一步操作
+    // reject参数表示要中断当前操作
+    return new Promise(function(resolve,reject){
+        setTimeout(function () {
+            resolve()
+        }, 1000);
+    })
+};
+
+let res = ajax().then(function(){
+    console.log('promise','timeout2');
+})
+
+console.log(res);
